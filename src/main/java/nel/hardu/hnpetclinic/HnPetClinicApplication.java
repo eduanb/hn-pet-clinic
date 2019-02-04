d265adef7e6d7a7cd80a3ec9f3d0c7a76f73634f
Hardu2203 <hardunelhn@gmail.com>
2019-02-04 23:09:48 +0200

    first commit

---
+++ src/main/java/nel/hardu/hnpetclinic/HnPetClinicApplication.java
@@ -0,0 +1,14 @@
+package nel.hardu.hnpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HnPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HnPetClinicApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/nel/hardu/hnpetclinic/HnPetClinicApplicationTests.java
@@ -0,0 +1,17 @@
+package nel.hardu.hnpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HnPetClinicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
